b5fa177c3ce718cdd66106913249b8eee022fb81
EikoNakashima <68436861+EikoNakashima@users.noreply.github.com>
2021-02-22 16:02:07 +0900

    l

---
+++ db/db.go
@@ -0,0 +1,97 @@
+package db
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+type Todo struct {
+	gorm.Model
+	Text   string
+	Status string
+}
+
+func gormConnect() *gorm.DB {
+	DBMS := "mysql"
+	USER := "test"
+	PASS := "12345678"
+	DBNAME := "test"
+	// MySQLだと文字コードの問題で"?parseTime=true"を末尾につける必要がある
+	CONNECT := USER + ":" + PASS + "@/" + DBNAME + "?parseTime=true"
+	db, err := gorm.Open(DBMS, CONNECT)
+
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
+//DB初期化
+func dbInit() {
+	db := gormConnect()
+	// if err != nil {
+	// 	panic("データベース開けず！（dbInit）")
+	// }
+	db.AutoMigrate(&Todo{})
+	defer db.Close()
+}
+
+//DB追加
+func dbInsert(text string, status string) {
+	db := gormConnect()
+	// if err != nil {
+	// 	panic("データベース開けず！（dbInsert)")
+	// }
+	db.Create(&Todo{Text: text, Status: status})
+	defer db.Close()
+}
+
+//DB更新
+func dbUpdate(id int, text string, status string) {
+	db := gormConnect()
+	// if err != nil {
+	// 	panic("データベース開けず！（dbUpdate)")
+	// }
+	var todo Todo
+	db.First(&todo, id)
+	todo.Text = text
+	todo.Status = status
+	db.Save(&todo)
+	db.Close()
+}
+
+//DB削除
+func dbDelete(id int) {
+	db := gormConnect()
+	// if err != nil {
+	// 	panic("データベース開けず！（dbDelete)")
+	// }
+	var todo Todo
+	db.First(&todo, id)
+	db.Delete(&todo)
+	db.Close()
+}
+
+//DB全取得
+func dbGetAll() []Todo {
+	db := gormConnect()
+	// if err != nil {
+	// 	panic("データベース開けず！(dbGetAll())")
+	// }
+	var todos []Todo
+	db.Order("created_at desc").Find(&todos)
+	db.Close()
+	return todos
+}
+
+//DB一つ取得
+func dbGetOne(id int) Todo {
+	db := gormConnect()
+	// if err != nil {
+	// 	panic("データベース開けず！(dbGetOne())")
+	// }
+	var todo Todo
+	db.First(&todo, id)
+	db.Close()
+	return todo
+}
--- main.go
+++ main.go
@@ -1,12 +1,79 @@
 package main
 
 import (
-	"fmt"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	return
-	var name string
-	name = "m"
-	fmt.Println("Hello", name)
+	router := gin.Default()
+	router.LoadHTMLGlob("views/*.html")
+
+	dbInit()
+
+	//Index
+	router.GET("/", func(ctx *gin.Context) {
+		todos := dbGetAll()
+		ctx.HTML(200, "index.html", gin.H{
+			"todos": todos,
+		})
+	})
+
+	//Create
+	router.POST("/new", func(ctx *gin.Context) {
+		text := ctx.PostForm("text")
+		status := ctx.PostForm("status")
+		dbInsert(text, status)
+		ctx.Redirect(302, "/")
+	})
+
+	//Detail
+	router.GET("/detail/:id", func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic(err)
+		}
+		todo := dbGetOne(id)
+		ctx.HTML(200, "detail.html", gin.H{"todo": todo})
+	})
+
+	//Update
+	router.POST("/update/:id", func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		text := ctx.PostForm("text")
+		status := ctx.PostForm("status")
+		dbUpdate(id, text, status)
+		ctx.Redirect(302, "/")
+	})
+
+	//削除確認
+	router.GET("/delete_check/:id", func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		todo := dbGetOne(id)
+		ctx.HTML(200, "delete.html", gin.H{"todo": todo})
+	})
+
+	//Delete
+	router.POST("/delete/:id", func(ctx *gin.Context) {
+		n := ctx.Param("id")
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			panic("ERROR")
+		}
+		dbDelete(id)
+		ctx.Redirect(302, "/")
+
+	})
+
+	router.Run()
 }
